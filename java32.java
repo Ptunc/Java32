ac1f863ee2a2a2ad8bc285c4ada6abe23f77ba89
Ptunc <spolentunc@gmail.com>
2022-10-25 14:15:55 +0300

    just commit

---
+++ java32.java
@@ -0,0 +1,22 @@
+package Java32;
+
+public class java32 {
+    static boolean isPalindrom (int number){
+        int temp=number,reverseNumber=0,lastNumber;
+        while (temp!=0){
+            lastNumber=temp%10;
+            reverseNumber=(reverseNumber*10)+lastNumber;
+            temp/=10;
+            if (temp==reverseNumber){
+                return true;
+            }else {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[]args){
+        System.out.println(isPalindrom(8765));
+    }
+
+}
